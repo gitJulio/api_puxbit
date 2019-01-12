84d5487eff6b6070fcb7321d44bfbaa5e806ea0d
Julio Bueso <julio.ezequiel@gmail.com>
2019-01-12 09:17:07 -0600

    Segundo comit

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 //Requires
-var express =require('express');
+var express = require('express');
 
 //Inicializar variables
-var app =express();
+var app = express();
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //Rutas
-app.get('/',(req, res, next)=>{
-  res.status(200).json({
-    ok:true,
-    mensaje:'peticion realizada correctamente'
-  })
+app.get('/', (req, res, next) => {
+  let data = {
+    ok: true,
+    mensaje: 'Petición realizada correctamente'
+  }
+  res.send(data)
+})
+
+app.get('/padres/:id', (req, res) => {
+  let padres = {
+    id: req.params.id,
+    nombre: 'Eduardo',
+    apellido: 'Calix'
+  }
+  res.send(padres)
+})
+
+app.post('/alumnos', (req, res) => {
+  let alumno = {
+    id: req.body.id,
+    nombre: 'Julio',
+    apellido: 'Bueso'
+  }
+
+  res.json(alumno)
 })
 
 //Escuchar peticiones
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Express server online");
 });
