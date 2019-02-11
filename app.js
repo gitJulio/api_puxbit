4cb7304f5228b91a9117cc217bc2ee9c6b383dc7
Julio Bueso <julio.ezequiel@gmail.com>
2019-02-11 09:04:31 -0600

    Api Puxbit Conexion data base

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
-//Requires
-var express = require('express');
-require('./config/config.js');
-//Inicializar variables
-var app = express();
-const bodyParser = require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+// const hbs = require('hbs');
+require('./config/config')
+// require('./hbs/helpers.js')
+// const pg = require('pg')
+const pg = require('./config/ps_conection')
+
+// app.use(require('./routes/historias'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
 }))
+// // parse application/json
+// app.use(express.static(__dirname + '/public'))
+//
+// //Express HBS engine -
+// hbs.registerPartials(__dirname + '/views/parciales');
+// app.set('view engine', 'hbs');
 
-// parse application/json
-app.use(bodyParser.json())
-
-//Rutas
-app.get('/', (req, res, next) => {
-  let data = {
-    ok: true,
-    mensaje: 'Petición realizada correctamente'
-  }
-  res.send(data)
+app.get('/', async (req, res) => {
+  let view_alumnos = await pg.func('public.ft_view_get_alumnos').catch(err => {
+    console.log(err);
+  })
+  console.log(view_historias);
 })
 
-app.get('/padres/:id', (req, res) => {
-  let padres = {
-    id: req.params.id,
-    nombre: 'Eduardo',
-    apellido: 'Calix'
-  }
-  res.send(padres)
-})
-
-app.post('/alumnos', (req, res) => {
-  let alumno = {
-    id: req.body.id,
-    nombre: 'Julio',
-    apellido: 'Bueso'
-  }
-
-  res.json(alumno)
-})
-
-//Escuchar peticiones
 app.listen(process.env.PORT, () => {
-  console.log("Express server online");
-});
+  console.log("Api Puxbit is up");
+})
--- config/config.js
+++ config/config.js
@@ -1,2 +1,14 @@
 // Puerto
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 5000;
+
+const config = {
+  db: {
+    host: '35.239.128.238',
+    port: "5432",
+    database: 'puxbit',
+    user: 'puxbit',
+    password: 'N1lointentes2019'
+  }
+};
+
+module.exports = config;
