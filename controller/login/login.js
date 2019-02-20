8b550dd3bdb978e83929f7b79a93b86aa5352c85
gitjulio <julio.ezequiel@gmail.com>
2019-02-19 23:24:49 -0600

    login

--- controller/login/login.js
+++ controller/login/login.js
@@ -4,37 +4,25 @@ const pg =require('../../config/ps_conection')
 exports.login=async function(req, res, next){
 
 let body=req.body;
-console.log("******");
-console.log(req.body.email)
-console.log("******");
+
 let usuario=
-await pg.func('public.ft_view_get_usuario',["bueso@outlook.com","123456"]).catch(err=>{
+await pg.func('public.ft_view_get_usuario',[body.email,body.password]).catch(err=>{
     res.status(500).send({
       error:err,
       status:500
     })
 })
 
+if (usuario && usuario !='') {
+  res.send({
+    ok:true
+  })
+}else{
+  res.status(400).send({
+    error:"usuario o contraseña invalidos"
+  })
+}
 
-// Usuario.findOne({email: body.mail}, (err, usuarioDB)=>{
-//     if (err) {
-//       return res.status(500).json({
-//         ok:false,
-//         err
-//       })
-//     }
-//
-//     if (!usuarioDB) {
-//       if (err) {
-//         return res.status(400).json({
-//           ok:false,
-//           err:{
-//             message:'Usuario o contraseña invalidos'
-//           }
-//         })
-//       }
-//     }
-// })
 
 
 }
